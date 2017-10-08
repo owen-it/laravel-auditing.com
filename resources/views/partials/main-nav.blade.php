3c6e175dfdf1860b95819f3c7afcb80cf1fd88fa
Antério vieira <anteriovieira@gmail.com>
2017-10-07 21:52:16 -0300

    Added Community gitter

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -4,8 +4,9 @@
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://github.com/owen-it/laravel-auditing">GitHub</a></li>
+		<li><a href="https://gitter.im/laravel-auditing">Gitter</a></li>
 		<li class="divider"></li>
 		<li><a href="/docs/{{ $currentVersion }}/contributing">Contributing</a></li>
-		<li><a href="/docs/{{ $currentVersion }}/problems">Having problems?</a></li>
+		<li><a href="/docs/{{ $currentVersion }}/problems">Having problems?</a></li> 
 	</ul>
 </li>
