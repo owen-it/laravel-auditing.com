fc01e9309a9cad6935481e4efc883414cd78279d
Antério vieira <anteriovieira@gmail.com>
2018-05-16 20:54:34 -0300

    feat(features) updated

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,3 +1,5 @@
+
+<li class="nav-docs"><a href="/docs/{!! $currentVersion !!}/introduction" target="_blank">Documentation</a></li>
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
