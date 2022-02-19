e2c426d52f4eb8c17c29a166acb20ea61295675a
Antério Vieira <anteriovieira@doit.local>
2022-02-19 00:46:08 -0300

    chore: adds algolia app id

--- config/algolia.php
+++ config/algolia.php
@@ -13,7 +13,7 @@ return [
     */
     'connections' => [
         'main' => [
-            'id' => 'laravel_auditing',
+            'id' => env('ALGOLIA_APP_ID', ''),
             'search_key' => env('ALGOLIA_SEARCH_KEY', ''),
             'key' => env('ALGOLIA_ADMIN_KEY', ''),
         ],
